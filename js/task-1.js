513fbf546c3c6f98d9574b0001be9ff15ec8ae35
Kateryna Khalamendyk <kateryna.khalamendyk@gmail.com>
2024-01-05 11:34:03 +0200

    add js hw 4

---
+++ js/task-1.js
@@ -0,0 +1,16 @@
+"use strict";
+function isEnoughCapacity(products, containerSize) {
+  let sum = 0;
+  for (const f of Object.values(products)) {
+    sum += f;
+  }
+  return sum <= containerSize;
+}
+
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)); // true
+
+console.log(isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)); // false
